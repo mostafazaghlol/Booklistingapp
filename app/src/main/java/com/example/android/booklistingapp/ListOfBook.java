1fbadea4e5be1886e69a0b5e237157dfa3c6f351
mostafa <mostafas.zaghlol@gmail.com>
2018-01-29 21:51:17 +0200

    finish asyncLoader

--- app/src/main/java/com/example/android/booklistingapp/ListOfBook.java
+++ app/src/main/java/com/example/android/booklistingapp/ListOfBook.java
@@ -1,19 +1,23 @@
 package com.example.android.booklistingapp;
 
+import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
-public class ListOfBook extends AppCompatActivity {
+public class ListOfBook extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<BookData>> {
     private final static String LOG_TAG = ListOfBook.class.getName();
+    private static final int Book_LOADER_ID = 1;
+    String url;
     private ArrayList<BookData> list = new ArrayList<>();
     private CustomListAdapter customListAdapter;
 
@@ -22,45 +26,52 @@ public class ListOfBook extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_book);
         Intent i = getIntent();
-        String url = i.getStringExtra("url");
+        url = i.getStringExtra("url");
         //Toast.makeText(this, "" + url, Toast.LENGTH_SHORT).show();
         customListAdapter = new CustomListAdapter(this, new ArrayList<BookData>());
-        ListOfBookAsyncTask listOfBookAsyncTask = new ListOfBookAsyncTask();
-        listOfBookAsyncTask.execute(url);
+//        ListOfBookAsyncTask listOfBookAsyncTask = new ListOfBookAsyncTask();
+//        listOfBookAsyncTask.execute(url);
 
         ListView list1 = (ListView) findViewById(R.id.list);
-        list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //زود هنا فتح ال كتاب على جوجل بلاى
-            }
-        });
+
         list1.setAdapter(customListAdapter);
-    }
 
-    class ListOfBookAsyncTask extends AsyncTask<String, Void, ArrayList> {
-        ArrayList<BookData> arrayList = new ArrayList<>();
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        @Override
-        protected ArrayList doInBackground(String... url) {
-            try {
-                arrayList = QuaryUtils.fetchData(url[0]);
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error with Do in background method");
-            }
-            return arrayList;
-        }
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
-        @Override
-        protected void onPostExecute(ArrayList data) {
-            try {
-                customListAdapter.clear();
-                if (data != null && !data.isEmpty()) {
-                    customListAdapter.addAll(data);
-                }
-            } catch (Exception e) {
-                Log.e(LOG_TAG, e.getMessage());
-            }
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+
+            loaderManager.initLoader(Book_LOADER_ID, null, this);
         }
     }
+
+    @Override
+    public Loader<List<BookData>> onCreateLoader(int i, Bundle bundle) {
+        // Create a new loader for the given URL
+        return new BookLoader(this, url);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<BookData>> loader, List<BookData> data) {
+        try {
+            customListAdapter.clear();
+            if (data != null && !data.isEmpty()) {
+                customListAdapter.addAll(data);
+            }
+        } catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<BookData>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        customListAdapter.clear();
+    }
+
 }
